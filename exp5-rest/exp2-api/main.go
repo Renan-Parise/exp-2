0c51156d117907492b1824794f6779ebc2631555
Renan S. Parise <43964756+Renan-Parise@users.noreply.github.com>
2024-03-26 13:13:45 -0300

    Update main.go

--- exp5-rest/exp2-api/main.go
+++ exp5-rest/exp2-api/main.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"api/handlers"
+	"api/utils"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	db, err := utils.GetDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r := gin.Default()
+
+	r.POST("/login", handlers.Login)
+	r.POST("/register", handlers.Register)
+
+	r.GET("/protected/resource", handlers.AuthMiddleware(), handlers.ProtectedResource)
+
+	http.ListenAndServe(":8080", r)
+}
